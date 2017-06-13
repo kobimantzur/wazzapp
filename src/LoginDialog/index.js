c6973d212da8438326513efa1fa9c1423d1e4ada
kobimantzur <kobimantzur@gmail.com>
2017-06-13 14:51:24 +0300

    adding html elements

--- src/LoginDialog/index.js
+++ src/LoginDialog/index.js
@@ -7,11 +7,13 @@ class LoginDialog extends Component {
 render(){
   return(
     <div>
-    
+    <h1>WazzApp</h1>
+    <h4>Use WazzApp, create your profile picture and enter your name</h4>
+    <input  type="text" placeholder="Enter your phone number"/>
+    <input type="text" placeholder="Enter your name"/>
     </div>
   );
 }
 
 }
-
 export default LoginDialog

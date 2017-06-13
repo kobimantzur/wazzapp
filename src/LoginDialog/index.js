8f4b72a1cc854333c283d4248989cc9dd5b287c5
Baruch <Ohad.Baruch@emc.com>
2017-06-13 15:23:27 +0300

    added basic login page

--- src/LoginDialog/index.js
+++ src/LoginDialog/index.js
@@ -4,16 +4,29 @@ import LoginImageCapture from '../LoginImageCapture';
 import * as firebase from 'firebase/app';
 
 class LoginDialog extends Component {
-render(){
-  return(
-    <div>
-    <h1>WazzApp</h1>
-    <h4>Use WazzApp, create your profile picture and enter your name</h4>
-    <input  type="text" placeholder="Enter your phone number"/>
-    <input type="text" placeholder="Enter your name"/>
-    </div>
-  );
-}
-
+  render(){
+    return(
+      <div>
+        <div className="login-dialog-container">
+          <h3 className="login-dialog-title">WazzApp</h3>
+          <p className="login-dialog-subtitle">Use WazzApp, create your profile picture and enter your name</p>
+          <p>
+            <input className="login-credentials" type="tel" placeholder="Enter your phone number" pattern="[0-9]"/>
+          </p>
+          <p>
+            <input className="login-credentials" type="text" placeholder="Enter your name"/>
+          </p>
+          <button className="button button-big login-button" disabled>Login</button>
+        </div>
+        <div className="login-dialog-validations">
+          <ol>
+            <li>Capture your profile image</li>
+            <li>Missing phone number</li>
+            <li>Missing Username</li>
+          </ol>
+        </div>
+      </div>
+    );
+  }
 }
 export default LoginDialog
